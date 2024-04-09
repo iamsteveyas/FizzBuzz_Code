ff8f10e8cbbc4a7e00efc3e04620496b4739e6e9
iamsteveyas <yasin.fallah.1988@outlook.com>
2024-04-10 00:29:15 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fizz = "fizz";
+            string buzz = "buzz";
+            string fizzbuzz = "fizzbuzz";
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(fizz);
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(buzz);
+                }
+                else if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(fizzbuzz);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
